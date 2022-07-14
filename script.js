57366b89fbef1eb3ef0e7a02cf1ee29fe53bb592
lvcg <flowfxdesignsonline@gmail.com>
2022-07-13 22:48:04 -0500

    dom manipulation

---
+++ script.js
@@ -0,0 +1,46 @@
+const body = document.body
+const div = document.querySelector('div')
+const spanHi =document.querySelector('#hi')
+const spanBye =document.querySelector('#bye')
+
+spanHi.classList.add("")
+spanHi.classList.remove("")
+spanHi.classList.toggle("hi3", true)
+
+spanHi.style.backgroundColor = "red"
+
+//console.log(spanHi.dataset.test)
+//spanHi.dataset.newName = "wtf"
+
+//console.log(spanHi.getAttribute(spanHi.getAttribute))
+//spanHI.removeAttribute('id')
+//spanHi.removeAttribute("id")
+//spanHi.setAttribute('id', 'bye')
+//spanHi.setAttribute('class', 'bye')
+//spanHi.id = 'bye'
+
+//spanBye.remove()
+//div.remove()
+//body.append("hello world")
+
+//const div = document.createElement('div')
+//body.append(div)
+
+//div.innerText = "hello world"
+//div.textContent = "hello world"
+
+//const div = document.querySelector("div")
+
+//console.log(div.textContent);
+//console.log(div.innerText)
+
+//div.innerHTML = "<strong> Hello world </strong>"   - Not recoomended
+
+//const strong = document.creatElement('strong')
+//strong.innerText = "Hello world"
+//div.append(strong)
+
+
+
+
+
